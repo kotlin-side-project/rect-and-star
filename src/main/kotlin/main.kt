81cd6a2cd5cf8b3bf7ef933d142eb8fffe6752b5
Jeewhan Ryu <distinct19@gmail.com>
2020-07-31 01:52:06 +0900

    feat: add stars to rect

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,16 +2,29 @@ import org.w3c.dom.HTMLDivElement
 import kotlin.browser.document
 
 fun main() {
-    document.addEventListener("DOMContentLoaded", { e ->
+    document.addEventListener("DOMContentLoaded", { _ ->
         val body = document.body!!
         val rect = document.createElement("div") as HTMLDivElement
-        rect.style.border = "1px solid black"
         rect.style.apply {
             border = "1px solid black"
             width = "100px"
             height = "100px"
+            boxSizing = "border-box"
+            display = "flex"
+            flexDirection = "column"
+            alignItems = "center"
         }
 
         body.appendChild(rect)
+
+        listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).forEach {
+            val child = document.createElement("div") as HTMLDivElement
+            val t = document.createTextNode("*".repeat(it))
+
+            child.style.height = "10px"
+
+            child.appendChild(t)
+            rect.appendChild(child)
+        }
     })
 }
